442cbe74242084a53006c1a3c2d4b7055140ab2e
Destro447 <chemseddine.nemeur@gmail.com>
2024-04-28 18:27:45 +0200

    Adding Database

---
+++ Backend/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Replace with your MySQL connection details
+DATABASE_URL = "mysql+mysqlconnector://user:password@host:port/database_name"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI,Request
 from fastapi.templating import Jinja2Templates
+from Backend.database import SessionLocal
 
 # Configure templates directory (adjust path as needed)
 templates = Jinja2Templates(directory="templates")
@@ -13,3 +14,13 @@ async def read_root(request: Request):
     # Pass context data to the template for dynamic rendering
     context = {"request": request, "title": "Simple Page"}
     return templates.TemplateResponse("index.html", context=context)
+
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
