6d23c72edb5079a0f84b5ac20f1db9ab23e0abf6
Destro447 <chemseddine.nemeur@gmail.com>
2024-04-29 00:45:00 +0200

    Database Connection

--- Backend/database.py
+++ Backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Replace with your MySQL connection details
-DATABASE_URL = ""
+DATABASE_URL = "mysql+pymysql://root:قخخف@localhost:3306/firstapplication"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ from Backend.database import SessionLocal,engine
 from sqlalchemy.orm import session
 from pydantic import BaseModel
 from typing import Annotated
-import Backend.models 
+import Backend.models as models
+from Backend.models import UserGreeting
 
 
 templates = Jinja2Templates(directory="templates")
@@ -12,18 +13,34 @@ app = FastAPI(templates=templates)
 
 models.Base.metadata.create_all(bind=engine)
 
-@app.get("/")
+"""@app.get("/")
 async def read_root(request: Request):
     # Pass context data to the template for dynamic rendering
     context = {"request": request, "title": "Simple Page"}
     return templates.TemplateResponse("index.html", context=context)
-
-
-
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+"""
 
+
+def get_db():
+  db = SessionLocal()
+  try:
+    greeting = db.query(UserGreeting).first()  
+    if not greeting:
+      greeting = "Erreur retrieving data from database."
+    yield db, greeting 
+  finally:
+    db.close()
+
+
+@app.get("/")
+async def read_root(request: Request, db: session = Depends(get_db)):
+
+  db, greeting = db
+  context = {"request": request, "title": "Simple Page", "greeting": greeting.Greeting}
+  return templates.TemplateResponse("index.html", context=context)
