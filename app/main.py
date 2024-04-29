d0b8de777f125ed725ee0f71d0f9383552d80b55
Destro447 <chemseddine.nemeur@gmail.com>
2024-04-29 18:00:32 +0200

    Final

--- app/main.py
+++ app/main.py
@@ -1,56 +1,26 @@
-"""from fastapi import FastAPI,Request,HTTPException,Depends,status
-from fastapi.templating import Jinja2Templates
 from Backend.database import SessionLocal,engine
-from sqlalchemy.orm import session
-from pydantic import BaseModel
-from typing import Annotated
-from Backend import models
-from Backend.models import UserGreeting
+from sqlalchemy import Column, String,Integer
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, session
+from fastapi import FastAPI,Request,Depends
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.ext.declarative import declarative_base
+import os
 
 
+    
 templates = Jinja2Templates(directory="templates")
 app = FastAPI(templates=templates)
 
-models.Base.metadata.create_all(bind=engine)
 
-def get_db():
-    db = SessionLocal()
-    try:
-        # Fetch the first UserGreeting record and access only the 'Greetings' field
-        greetings = db.query(UserGreeting).first()
-
-
-        if greetings:
-            greetings = greetings.Greetings  # Access the 'Greetings' field directly
-        else:
-            greetings = "Erreur retrieving data from database."
-
-        yield db, greetings
-    finally:
-        db.close()
-
-
-
-@app.get("/")
-async def read_root(request: Request, db: session = Depends(get_db)):
-
-  db, greetings = db
-  context = {"request": request, "title": "Simple Page", "greeting": greetings}
-  return templates.TemplateResponse("index.html", context=context)
-"""
-
-
-import os
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+"""DATABASE_URL = os.getenv("mysql+pymysql://root:قخخف@localhost:3306/firstapplication")"""
+DATABASE_URL="mysql+pymysql://root:قخخف@localhost:3306/firstapplication"
 
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(DATABASE_URL)
+Base = declarative_base()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -69,3 +39,17 @@ def get_db():
         yield db, greetings
     finally:
         db.close()
+
+@app.get("/")
+async def read_root(request: Request, db: session = Depends(get_db)):
+
+  db, greetings = db
+  context = {"request": request, "title": "Simple Page", "greeting": greetings}
+  return templates.TemplateResponse("index.html", context=context)
+
+
+class UserGreeting(Base):
+    __tablename__ = "greet"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    Greetings = Column(String(200))
